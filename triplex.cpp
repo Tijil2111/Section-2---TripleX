155ef36f760193ef3e5e4f90a29dc032ce67838f
Tijil Agrawal <tijil2008@gmail.com>
2021-11-04 19:28:35 +0530

    Comments in C++

--- triplex.cpp
+++ triplex.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
+
+    // Print welcome message to the console 
     std::cout <<"You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout <<"Enter the correct code to continue.......";
 
+
+    // Declaring variables 
     const int a = 4;
     const int b = 5;
     const int c = 6;
@@ -14,6 +18,9 @@ int main()
     const int sum = a+b+c;
     const int product = a*b*c;
 
+
+
+    // Printing sum and product in console 
     std::cout << std::endl;
     std::cout << sum;
     std::cout << std::endl;
