c0b5110b0d7ad490b74db86549901ae7c03a7180
Tijil Agrawal <tijil2008@gmail.com>
2021-11-04 19:17:02 +0530

    const and assigning values in C++

--- triplex.cpp
+++ triplex.cpp
@@ -6,12 +6,13 @@ int main()
     std::cout << std::endl;
     std::cout <<"Enter the correct code to continue.......";
 
-    int a = 4;
-    int b = 5;
-    int c = 6;
+    const int a = 4;
+    const int b = 5;
+    const int c = 6;
+    
 
-    int sum = a+b+c;
-    int product = a*b*c;
+    const int sum = a+b+c;
+    const int product = a*b*c;
 
     std::cout << std::endl;
     std::cout << sum;
