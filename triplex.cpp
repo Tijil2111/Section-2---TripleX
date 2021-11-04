2c2d3a85a33c5228b6e64d7d3028ccfeddc86d03
Tijil Agrawal <tijil2008@gmail.com>
2021-11-04 15:53:13 +0530

    Variables in C++

--- triplex.cpp
+++ triplex.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
-int main(){
-    
+int main()
+{
     std::cout <<"You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout <<"Enter the correct code to continue.......";
+
+    int a = 4;
+    int b = 5;
+    int c = 6;
+
+    int sum = a+b+c;
+    int product = a*b*c;
+
+    std::cout << std::endl;
+    std::cout << sum;
+    std::cout << std::endl;
+    std::cout << product;
 
     return 0;
 }
